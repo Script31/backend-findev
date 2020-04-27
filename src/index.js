6a30356e1d476f29bc2bbda8bad28e7cf1ff3ceb
Elves Trindade <elvestrindade31@gmail.com>
2020-04-27 08:17:40 +0100

    backend all file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(
+  'mongodb+srv://posts:<eversay>@salvation-2onje.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(4000, console.log('server is running'));
